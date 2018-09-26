79c9b225526acb69e96c481b4971a3e68868d2b4
sirisurab <siri.surab@gmail.com>
2018-09-25 19:19:48 -0500

    minio-fput

--- src/utils/messaging.py
+++ src/utils/messaging.py
@@ -35,7 +35,7 @@ def push_tasks_to_q(tasks: List[str], queue: str) -> None:
 def del_from_q(msg: str, queue: str) -> None:
     r = get_client()
     r.lrem(queue, msg, 1)
-    print('deleted message '+msg+' from '+queue)
+    print('deleted message '+str(msg)+' from '+queue)
     return
 
 
