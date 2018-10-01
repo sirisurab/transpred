993457d6a3ce0964eec9b0704c6ee6ce247e005b
sirisurab <siri.surab@gmail.com>
2018-10-01 13:37:52 -0500

    taxi-zones

--- src/data_clean/tasks.py
+++ src/data_clean/tasks.py
@@ -64,9 +64,9 @@ def add_cab_zone(df) -> pd.DataFrame:
                 path_prefix: str = '/tmp/'
                 for file in taxi_zone_files:
                     file_obj: Object = ps.get_file(bucket='others', filename=file, filepath=path_prefix+file)
-                    print('fetched taxi zones shape file %s' % str(file_obj))
+                    #print('fetched taxi zones shape file %s' % str(file_obj))
                 taxi_zone_df: GeoDataFrame = read_file(path_prefix+taxi_zone_files[0])
-                print('taxi zones GeoDF '+str(taxi_zone_df.head(1)))
+                #print('taxi zones GeoDF '+str(taxi_zone_df.head(1)))
                 print('taxi zones GeoDF columns '+str(taxi_zone_df.columns))
                 geometry: List[Point] = [Point(xy) for xy in zip(df.dolatitude, df.dolongitude)]
                 df = df.drop(['dolatitude', 'dolongitude'], axis=1)
@@ -74,7 +74,7 @@ def add_cab_zone(df) -> pd.DataFrame:
                 print('taxi zones shapefile crs '+str(crs))
                 #crs: Dict[str, str] = {'init': 'epsg:4326'}
                 geodf: GeoDataFrame = GeoDataFrame(df, crs=crs, geometry=geometry)
-                print('converted df to GeoDF '+str(geodf.head(1)))
+                #print('converted df to GeoDF '+str(geodf.head(1)))
                 print('converted df to GeoDF '+str(geodf.columns))
                 geodf = sjoin(geodf, taxi_zone_df, how='left', op='within')
                 print('after spatial join with taxi zones '+str(geodf.head(1)))
