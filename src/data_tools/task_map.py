b17026dcd5e4065a387672d46a863d9a8fe65d80
sirisurab <siri.surab@gmail.com>
2018-10-01 16:57:21 -0500

    taxi-zones

--- src/data_tools/task_map.py
+++ src/data_tools/task_map.py
@@ -20,7 +20,7 @@ task_type_map: Dict = {
                                         },
                                 'dates': {
                                         'parse': True,
-                                        'in_cols': [0],
+                                        'in_cols': ['Lpep_dropoff_datetime'],
                                         'out_col': 'dodatetime',
                                         'parser': row_ops.clean_cabs_dt
                                         },
