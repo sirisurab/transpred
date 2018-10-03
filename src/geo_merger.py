80f74dc14acfa43592cba374632f2a79633f2186
sirisurab <siri.surab@gmail.com>
2018-10-03 10:32:50 -0500

    geo-merge

--- src/geo_merger.py
+++ src/geo_merger.py
@@ -2,9 +2,11 @@ import sys
 from data_tools import file_io
 from geopandas import GeoDataFrame, sjoin
 from pandas import DataFrame
+from math import sin, pi
 
 GEOMERGED_BUCKET: str = 'geo-merged'
 REFBASE_BUCKET: str = 'ref-base'
+NYC_LATITUDE: float = 40.7128
 
 
 def geo_merge(buffer_radius: float) -> bool:
@@ -16,9 +18,15 @@ def geo_merge(buffer_radius: float) -> bool:
         stations_df: GeoDataFrame = file_io.fetch_geodf_from_zip(filename=st_filename,
                                                                   zipname=st_zipname,
                                                                   bucket=REFBASE_BUCKET)
+
+        # covert buffer_radius from miles to degrees
+        gc_radius_miles: float = 3963 - 13 * sin(NYC_LATITUDE * pi/180)
+        buffer_degrees: float = buffer_radius / gc_radius_miles
+
         # add circular buffer around each station
-        stations_df['circle'] = stations_df.geometry.buffer(buffer_radius)
-        stations_df.rename(columns={'geometry': 'point'}, inplace=True).set_geometry('circle', inplace=True)
+        stations_df['circle'] = stations_df.geometry.buffer(buffer_degrees)
+        stations_df.rename(columns={'geometry': 'point'}, inplace=True)
+        stations_df.set_geometry('circle', inplace=True)
 
         # load taxi_zones data
         tz_zipname: str = 'taxi_zones.zip'
