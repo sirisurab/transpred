f18e94e70a35f8ea21c63601081251ed63d1f7ed
sirisurab <siri.surab@gmail.com>
2018-10-08 09:32:54 -0500

    ycabs

--- src/data_clean/tasks.py
+++ src/data_clean/tasks.py
@@ -578,6 +578,7 @@ def perform_dask(task_type: str, years: List[str]) -> bool:
                     dd.to_parquet(df=df,
                               path=s3_out_url,
                               engine='fastparquet',
+                              compute=True,
                               compression='snappy',
                               storage_options=s3_options)
 
