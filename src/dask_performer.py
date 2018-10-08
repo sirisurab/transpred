34c003d3695cdb8be8283825ee610494cc838ae2
sirisurab <siri.surab@gmail.com>
2018-10-07 23:35:45 -0500

    ycabs-resample-2016

--- src/dask_performer.py
+++ src/dask_performer.py
@@ -5,6 +5,7 @@ from data_resample import tasks as rs_tasks
 from data_clean import tasks as cl_tasks
 from typing import List
 from dask.distributed import Client
+import time
 
 if __name__ == '__main__':
     task_type: str = sys.argv[1]
@@ -13,7 +14,17 @@ if __name__ == '__main__':
     status: bool = ps.create_bucket(task_map.task_type_map[task_type]['out'])
     if status:
         # initialize distributed client
-        client: Client = Client('dscheduler:8786')
+        while True:
+            try:
+                client: Client = Client('dscheduler:8786')
+            except TimeoutError:
+                time.sleep(2)
+                continue
+            except Exception as err:
+                raise err
+            else:
+                break
+
         task_prefix: str = task_type.split('-', 1)[0]
         if task_prefix == 'rs':
             status = rs_tasks.perform_dask(task_type, years)
