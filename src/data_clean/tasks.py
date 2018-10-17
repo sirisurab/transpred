4ed1ab9400d847a4e1ace23f88ae7517e017b8e4
sirisurab <siri.surab@gmail.com>
2018-10-16 21:23:25 -0500

    dask-rs

--- src/data_clean/tasks.py
+++ src/data_clean/tasks.py
@@ -348,7 +348,7 @@ def clean_cabs_at_path(special: bool, s3_in_url: str, s3_out_url: str, s3_option
                       path=s3_out_url,
                       engine='fastparquet',
                       compute=True,
-                      compression='snappy',
+                      compression='GZIP',
                       storage_options=s3_options)
         del df
 
