908609c5b4280da6c97a674f56591b2679f4920d
sirisurab <siri.surab@gmail.com>
2018-10-18 13:47:35 -0500

    spatial-buffers

--- src/geo_merger.py
+++ src/geo_merger.py
@@ -7,6 +7,8 @@ from geog import propagate
 from numpy import linspace, arange, ndarray
 import seaborn as sns
 from shapely.geometry import Polygon
+from typing import Tuple
+from pandas import Series
 
 GEOMERGED_PATH: str = 'geo-merged/'
 REFBASE_BUCKET: str = 'ref-base'
@@ -39,28 +41,31 @@ def make_plots(buffer_radius_miles: float, stations_geodf: GeoDataFrame, taxi_zo
     return status
 
 
-def create_spatial_joins(buffer_radius_miles: float, stations_geodf: GeoDataFrame, taxi_zone_df: GeoDataFrame, links_df: GeoDataFrame) -> bool:
+def create_spatial_joins(buffer_radius_miles: float, stations_geodf: GeoDataFrame, taxi_zone_df: GeoDataFrame, links_df: GeoDataFrame, prev_buffer_ids: Tuple[Series, Series]) -> Tuple[Series, Series]:
     # perform spatial join
     # between stations (buffer circles) and taxi-zones polygons
     stations_cabs_df: GeoDataFrame = sjoin(stations_geodf, taxi_zone_df, how='left', op='intersects')
-
-    # write joined file (as csv without geometry columns) to geo-merged bucket
     stations_cabs_df = stations_cabs_df[['station_id', 'stop_id', 'stop_name', 'tsstation', 'borough', 'LocationID']]
     stations_cabs_df.rename(columns={'LocationID': 'locationid'}, inplace=True)
-    geomerged_file: str = GEOMERGED_PATH + str(buffer_radius_miles) + '/cabs.csv'
-    status_1: bool = file_io.write_csv(df=stations_cabs_df, bucket=REFBASE_BUCKET, filename=geomerged_file)
 
     # perform spatial join
     # between stations (buffer circles) and traffic_links lines
     stations_traffic_df: GeoDataFrame = sjoin(stations_geodf, links_df, how='left', op='intersects')
-
-    # write joined file (as csv without geometry columns) to geo-merged bucket
     stations_traffic_df = stations_traffic_df[['station_id', 'stop_id', 'stop_name', 'tsstation', 'borough', 'linkid']]
-    # df.rename(columns={'LocationID': 'dolocationid'}, inplace=True)
+
+    # save all cab location ids and traffic link ids from current buffer circle (to exclude from next buffer)
+    new_buffer_ids: Tuple[Series, Series] = (stations_cabs_df['locationid'], stations_traffic_df['linkid'])
+
+    # exclude previous buffer cab location ids and traffic link ids from current buffer circle, before writing to file
+    stations_cabs_df = stations_cabs_df[stations_cabs_df['locationid'] not in prev_buffer_ids[0]]
+    stations_traffic_df = stations_traffic_df[stations_traffic_df['linkid'] not in prev_buffer_ids[1]]
+    # write files
+    geomerged_file: str = GEOMERGED_PATH + str(buffer_radius_miles) + '/cabs.csv'
+    status_1: bool = file_io.write_csv(df=stations_cabs_df, bucket=REFBASE_BUCKET, filename=geomerged_file)
     geomerged_file = GEOMERGED_PATH + str(buffer_radius_miles) + '/traffic.csv'
     status_2: bool = file_io.write_csv(df=stations_traffic_df, bucket=REFBASE_BUCKET, filename=geomerged_file)
 
-    return status_1 and status_2
+    return new_buffer_ids
 
 
 def geo_merge(buffer_radii: ndarray) -> bool:
@@ -88,6 +93,9 @@ def geo_merge(buffer_radii: ndarray) -> bool:
                                                           bucket=REFBASE_BUCKET)
 
     status_1: bool = False
+    cab_locationids: Series = Series()
+    traffic_linkids: Series = Series()
+    prev_buffer_ids: Tuple[Series, Series] = (cab_locationids, traffic_linkids)
     for radius in buffer_radii:
         status_1 = False
         try:
@@ -123,10 +131,11 @@ def geo_merge(buffer_radii: ndarray) -> bool:
                                             links_df=links_df)
 
                 # perform spatial join
-                #status_1 = create_spatial_joins(buffer_radius_miles=buffer_radius_miles,
-                #                                stations_geodf=stations_geodf,
-                #                                taxi_zone_df=taxi_zone_df,
-                #                                links_df=links_df)
+                prev_buffer_ids = create_spatial_joins(buffer_radius_miles=buffer_radius_miles,
+                                                       stations_geodf=stations_geodf,
+                                                       taxi_zone_df=taxi_zone_df,
+                                                       links_df=links_df,
+                                                       prev_buffer_ids=prev_buffer_ids)
 
             except Exception as err:
                 print('Error in geo_merge %(radius)s' % {'radius': str(buffer_radius_miles)})
