271f7206e647d6dfd64ecb8646840f1b16c8e09e
sirisurab <siri.surab@gmail.com>
2018-10-19 03:52:21 -0500

    plots-weighted

--- src/plotter.py
+++ src/plotter.py
@@ -31,7 +31,7 @@ def create_plot(df1: DataFrame, varcol1: str, label1: str, df2: DataFrame, varco
         for _, group in df2.reset_index().groupby([datecol, weight_col]):
             df = group[varcol2].apply(mean).unstack(weight_col)
             weight = df[weight_col].iloc[0]
-            size = 1 / 2 / weight
+            size = 1 / (2 * weight)
             sns.lineplot(data=df[varcol2], ax=ax1, color='coral', size=size)
     else:
         sns.lineplot(data=df2[varcol2], ax=ax1, color='coral', label=label2, legend='brief')
