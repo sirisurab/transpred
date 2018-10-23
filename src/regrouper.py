4b7211d482fbd5de0e3b9909792705aa8c88f1a7
sirisurab <siri.surab@gmail.com>
2018-10-23 16:55:41 -0500

    ts-fare

--- src/regrouper.py
+++ src/regrouper.py
@@ -101,7 +101,7 @@ def regroup_dask(task_type: str, years: List[str], resample_freq: str, filter_ke
 
         print('read files from in bucket and concat-ted into one df')
         if task_type == 'rg-tsfare':
-            s3_sub_path = resample_freq + '/' + filter_key+filter_val + '/'
+            s3_sub_path = resample_freq + '/'
         df.groupby(split_by).apply(partial(write_group_to_csv, split_by=split_by, out_bucket=out_bucket, out_path=s3_sub_path), meta=('int')).compute()
 
     except Exception as err:
