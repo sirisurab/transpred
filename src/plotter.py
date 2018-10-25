d323ad378cb6204d2346400ff03e29e383048678
sirisurab <siri.surab@gmail.com>
2018-10-25 12:12:16 -0500

    ts-fare

--- src/plotter.py
+++ src/plotter.py
@@ -51,7 +51,7 @@ def create_plot(df1: DataFrame, varcol1: str, label1: str, df2: DataFrame, varco
 
     if multiplot:
         df2 = df2.reset_index()
-        g = sns.lineplot(x='date', y=varcol2, estimator=None, ax=ax1, label=label2, hue=multicol, data=df2)
+        g = sns.lineplot(x='date', y=varcol2, estimator=None, ax=ax1, hue=multicol, data=df2, kwargs={'linewidth':.7})
     else:
         g = sns.lineplot(data=df2[varcol2], ax=ax1, color=COLOR1, label=label2)
     ax.set_ylabel(label1)
@@ -74,7 +74,7 @@ def create_reg_plot(df: DataFrame, varcol1: str, label1: str, varcol2: str, labe
         df[varcol2] = df[varcol2] / (RELPLOT_SZ_MULT * df[weight_col])
     df = row_operations.drop_outliers(df=df, col=varcol2)
     if multiplot:
-        g = sns.scatterplot(x=varcol1, y=varcol2, data=df, ax=ax, hue=multicol)
+        g = sns.scatterplot(x=varcol1, y=varcol2, data=df, ax=ax, hue=multicol, kwargs={'s':10})
     else:
         g = sns.regplot(x=varcol1, y=varcol2, data=df, ax=ax, color=COLOR1, scatter_kws={'s':10}, line_kws={'linewidth':.8})
     ax.set_xlabel(label1)
