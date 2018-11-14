18a5f03f2a30cceef6d6a7e1bf8a421f512a8dd1
sirisurab <siri.surab@gmail.com>
2018-11-13 21:49:51 -0600

    resample without weekday

--- src/data_resample/tasks.py
+++ src/data_resample/tasks.py
@@ -272,7 +272,7 @@ def perform_dask(task_type: str, years: List[str]) -> bool:
 
                 # resample using frequency and aggregate function specified
                 df = df.groupby([pd.Grouper(key=index_col, freq=resample_freq)] + grouper_cols)[cols]. \
-                    apply(aggr_func, meta=meta).reset_index()
+                    apply(aggr_func, meta=meta).reset_index().compute()
                 # df = df.resample(resample_freq).sum()
                 # print('after resampling')
 
@@ -287,8 +287,8 @@ def perform_dask(task_type: str, years: List[str]) -> bool:
             dd.to_parquet(df=df,
                           path=s3_out_url,
                           engine='fastparquet',
-                          compute=True,
-                          write_index=True,
+                          #compute=True,
+                          #write_index=True,
                           compression='lz4',
                           storage_options=s3_options)
 
